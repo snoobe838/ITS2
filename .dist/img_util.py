a6a2869f6dccff04d6bb59b0f28fc0936331534b
Shubham Pareek <shubhampareek838@gmail..com>
2023-08-11 11:45:31 +0530

    its2

--- .dist/img_util.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def imshow(im, title,type = '' ):
-    if type:
-        plt.imshow(im, cmap= type)
-    else:
-        plt.imshow(im)
-    plt.title(title)
-    plt.axis('off')
